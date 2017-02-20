1ecfda27bff1e60b0c90a4e0f872f7909a7c6d74
Colin Harrington <colin.harrington@gmail.com>
2017-02-20 09:35:30 -0600

    Added test around the git.properties file

---
+++ complete/src/test/groovy/demo/GitPropertiesSpec.groovy
@@ -0,0 +1,25 @@
+package demo
+
+import spock.lang.Specification
+
+class GitPropertiesSpec extends Specification {
+
+    void "inspecting git.properties"() {
+        given:
+        Properties props = new Properties()
+        File gitProperties = new File("build/resources/main/git.properties")
+
+        when:
+        props.load(gitProperties.newDataInputStream())
+
+        then:
+        props['git.branch'] == 'master'
+        props['git.commit.user.name'] == "Colin Harrington"
+        props['git.commit.user.email'] == "colin.harrington@gmail.com"
+        props['git.commit.id'].matches('^[0-9a-f]+$')
+        props['git.commit.id'].startsWith(props['git.commit.id.abbrev'])
+        props['git.commit.time']
+        props['git.commit.message.short']
+        props['git.commit.message.full']
+    }
+}
